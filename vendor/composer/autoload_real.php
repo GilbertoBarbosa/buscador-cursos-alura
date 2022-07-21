be2726fc107eceb702829d81a4d790878a95277b
Gilberto Barbosa <gisantos555@gmail.com>
2022-07-21 15:11:26 -0300

    Versão 1

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitb54eae9e489c4fb2cadf2c7bc4cd9ffe
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitb54eae9e489c4fb2cadf2c7bc4cd9ffe', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitb54eae9e489c4fb2cadf2c7bc4cd9ffe', 'loadClassLoader'));
@@ -31,6 +33,25 @@ class ComposerAutoloaderInitb54eae9e489c4fb2cadf2c7bc4cd9ffe
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInitb54eae9e489c4fb2cadf2c7bc4cd9ffe::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireb54eae9e489c4fb2cadf2c7bc4cd9ffe($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequireb54eae9e489c4fb2cadf2c7bc4cd9ffe($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
